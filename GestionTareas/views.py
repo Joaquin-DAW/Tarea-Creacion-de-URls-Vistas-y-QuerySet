19357e050b433193ff37609c92b674f8b51fb8d8
Joaquin Cabezas <joaquin.cabezas.berjillos.al@iespoligonosur.org>
2024-10-29 10:10:07 +0100

    Correcciones

--- GestionTareas/views.py
+++ GestionTareas/views.py
@@ -69,10 +69,10 @@ def mi_error_404(request,exception=None):
     return render(request, 'errores/404.html',None,None,404)
 
 def mi_error_400(request, exception=None):
-    return render(request, 'errors/400.html', None,None,400)
+    return render(request, 'errores/400.html', None,None,400)
 
 def mi_error_403(request, exception=None):
-    return render(request, 'errors/403.html', None,None,403)
+    return render(request, 'errores/403.html', None,None,403)
 
 def mi_error_500(request):
-    return render(request, 'errors/500.html', None,None,500)
+    return render(request, 'errores/500.html', None,None,500)
